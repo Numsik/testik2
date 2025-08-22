e37f477a9ee9d1a45ed6576d956095e4bc97563e
nums <honzik.klusek.myto@gmail.com>
2025-08-22 11:49:12 +0200

    verse 13

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ console.log('ahoj');
 console.log('juj');
 
 console.log('version 2');
+console.log("upravatymrdko")
+console.log("upravatymrdko")
+
+console.log("upravatymrdko")
+console.log("upravatymrdko")
+console.log("upravatymrdko")
+console.log("upravatymrdko")
+console.log("upravatymrdko")
+
+console.log("upravatymrdko")
+
+console.log("upravatymrdko")
+console.log("upravatymrdko")
+console.log("upravatymrdko")
--- version.js
+++ version.js
@@ -2,6 +2,7 @@
 for (i = 0; i < 10; i++){
     console.log('realshiiit');
     console.log("relahistuihjpojpnp")
+    console.log("upravatymrdko")
 }
 
 
